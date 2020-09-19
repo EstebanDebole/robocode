42a087c2e8f4e937032615bdcf24df39a0d780f2
esteban <estebandebole@gmail.com>
2020-09-19 18:08:46 -0300

    refactoring

--- src/laboratorio/LaboRobot.java
+++ src/laboratorio/LaboRobot.java
@@ -1,41 +1,42 @@
 package laboratorio;
+
 import robocode.*;
 
 // API help : http://robocode.sourceforge.net/docs/robocode/robocode/JuniorRobot.html
 
 public class LaboRobot extends JuniorRobot {
 
-	private LaboRobotStrategy strategy; 
-	
-	@Override	
+	private LaboRobotStrategy strategy;
+
+	@Override
 	public void run() {
-		strategy = new LaboRobotStrategyTwo(this); //Set the initial strategy
-		boolean strategyChange = false;
-		setColors(red,black,black);
+		strategy = new LaboRobotStrategyTwo(this); // Set the initial strategy
+		boolean strategyChanged = false;
+		setColors(red, black, black);
 		while (true) {
 			strategy.runStrategy();
-			if ((!strategyChange) && (this.others == 1)) {
+			if ((!strategyChanged) && (this.others == 1)) {
 				strategy = new LaboRobotStrategyOne(this);
-				strategyChange = true;
+				strategyChanged = true;
 			}
 		}
 	}
 
-	//when the radar detects another robot. 
-	//When this event occurs the scannedDistance, scannedAngle, scannedBearing, and scannedEnergy 
-	//field values are automatically updated.
+	// when the radar detects another robot.
+	// When this event occurs the scannedDistance, scannedAngle, scannedBearing, and scannedEnergy
+	// field values are automatically updated.
 	@Override
 	public void onScannedRobot() {
 		strategy.onScannedRobot();
 	}
 
-	//onHitByBullet: What to do when you're hit by a bullet
+	// onHitByBullet: What to do when you're hit by a bullet
 	@Override
 	public void onHitByBullet() {
 		strategy.onHitByBullet();
 	}
-	
-	//onHitWall: What to do when you hit a wall
+
+	// onHitWall: What to do when you hit a wall
 	@Override
 	public void onHitWall() {
 		strategy.onHitWall();
--- src/laboratorio/LaboRobotStrategyOne.java
+++ src/laboratorio/LaboRobotStrategyOne.java
@@ -1,88 +1,86 @@
 package laboratorio;
 
 public class LaboRobotStrategyOne implements LaboRobotStrategy {
-	
-	public LaboRobot robot;
-	public boolean direction = true;
-	public boolean first = true;
-	
+
+	private LaboRobot robot;
+	private boolean directionForward = true;
+	private boolean isFirstTime = true;
+
 	public LaboRobotStrategyOne(LaboRobot robot) {
 		this.robot = robot;
 	}
-	
+
 	@Override
 	public void runStrategy() {
-		this.hello();
-		this.erraticMove();	
-		//360° mapping trigger the event onScannedRobot()
+		this.firstMove();
+		this.erraticMove();
+		// 360° mapping trigger the event onScannedRobot()
 		robot.turnGunLeft(360);
 	}
 
-	
 	@Override
 	public void onScannedRobot() {
 		int distance = robot.scannedDistance;
-		//robot.out.println("distance: "+distance);
+		// robot.out.println("distance: "+distance);
 		int angle = robot.scannedAngle;
-		//robot.out.println("angle: "+angle);
+		// robot.out.println("angle: "+angle);
 		int velocity = robot.scannedVelocity;
-		//robot.out.println("velocity: "+velocity);
-		//robot.out.println("heading: "+robot.scannedHeading); 
-		
+		// robot.out.println("velocity: "+velocity);
+		// robot.out.println("heading: "+robot.scannedHeading);
+
 		if (distance > 350) {
-			this.attackMove(angle,distance);
-		} else if ((robot.gunReady) && (velocity==0)) {
-			this.attack(angle,distance);
+			this.attackMove(angle, distance);
+		} else if ((robot.gunReady) && (velocity == 0)) {
+			this.attack(angle, distance);
 		}
 	}
 
 	@Override
 	public void onHitByBullet() {
 		int angle = robot.hitByBulletAngle;
-		robot.turnTo(angle+90);
-		robot.turnGunTo(angle);		
+		robot.turnTo(angle + 90);
+		robot.turnGunTo(angle);
 	}
 
 	@Override
 	public void onHitWall() {
-		robot.turnTo(robot.heading+90);
+		robot.turnTo(robot.heading + 90);
 	}
-	
-	
+
 	private void attack(int angle, int distance) {
-		robot.turnGunTo(angle);	
+		robot.turnGunTo(angle);
 		if (distance < 200) {
 			robot.fire(3);
 		} else if (distance < 250) {
 			robot.fire(2);
-		}else {
+		} else {
 			robot.fire(1);
 		}
 	}
-	
+
 	private void attackMove(int angle, int distance) {
 		robot.turnTo(angle);
 		if (distance > 200) {
 			robot.ahead(300);
-		}else {
+		} else {
 			robot.ahead(150);
 		}
 	}
-	
+
 	private void erraticMove() {
-		if (direction) {
+		if (directionForward) {
 			robot.ahead(75);
 		} else {
 			robot.back(75);
 		}
-		direction = !direction;
+		directionForward = !directionForward;
 	}
-	
-	private void hello(){
-		if (first) {
+
+	private void firstMove() {
+		if (isFirstTime) {
 			robot.turnTo(180);
 			robot.out.println("LaboRobotStrategyOne");
-			first = false;
+			isFirstTime = false;
 		}
 	}
 
