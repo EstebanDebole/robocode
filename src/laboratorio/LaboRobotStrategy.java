4254dc75f2a8fff91684ddcdf73c78a57a384127
esteban <estebandebole@gmail.com>
2020-09-19 18:08:46 -0300

    refactoring

--- src/laboratorio/LaboRobotStrategy.java
+++ src/laboratorio/LaboRobotStrategy.java
@@ -1,10 +1,13 @@
 package laboratorio;
 
 public interface LaboRobotStrategy {
-	
+
 	void runStrategy();
+
 	void onScannedRobot();
+
 	void onHitByBullet();
+
 	void onHitWall();
 
 }
--- src/laboratorio/LaboRobotStrategyTwo.java
+++ src/laboratorio/LaboRobotStrategyTwo.java
@@ -1,49 +1,49 @@
 package laboratorio;
 
 public class LaboRobotStrategyTwo implements LaboRobotStrategy {
-	
-	public LaboRobot robot;
-	public boolean first = true;
-	
+
+	private LaboRobot robot;
+	private boolean isFirstTime = true;
+
 	public LaboRobotStrategyTwo(LaboRobot robot) {
 		this.robot = robot;
 	}
-	
+
 	@Override
 	public void runStrategy() {
-		this.hello();
+		this.firstMove();
 		robot.ahead(300);
 	}
-	
+
 	@Override
 	public void onScannedRobot() {
 		int distance = robot.scannedDistance;
-		robot.out.println("distance: "+distance);
+		robot.out.println("distance: " + distance);
 		if ((distance != -1) && (distance < 150)) {
-			robot.turnTo(robot.heading+90);
+			robot.turnTo(robot.heading + 90);
 		}
 	}
 
 	@Override
 	public void onHitByBullet() {
 		int angle = robot.hitByBulletBearing;
-		robot.out.println("angle: "+angle);
-		if (angle==0) {
+		robot.out.println("angle: " + angle);
+		if (angle == 0) {
 			robot.fire(3);
 		}
 	}
 
 	@Override
 	public void onHitWall() {
-		robot.turnTo(robot.heading+90);
-	}	
-	
-	private void hello(){
-		if (first) {
+		robot.turnTo(robot.heading + 90);
+	}
+
+	private void firstMove() {
+		if (isFirstTime) {
 			robot.turnTo(90);
 			robot.out.println("LaboRobotStrategyTwo");
-			first = false;
+			isFirstTime = false;
 		}
 	}
-	
+
 }
